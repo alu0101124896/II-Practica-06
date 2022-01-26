36afe0da178f9186dd7f966291e49981f186a589
Sergio Tabares Hernández <alu0101124896@ull.edu.es>
2022-01-26 20:05:23 +0000

    README.md añadido

---
+++ README.md
@@ -0,0 +1,5 @@
+# Práctica 3
+
+Ejemplo de ejecución:
+
+![Ejecucion.jpg](./img/Ejecucion.jpg)
--- scripts/Accelerometer.cs
+++ scripts/Accelerometer.cs
@@ -12,19 +12,19 @@ public class Accelerometer : MonoBehaviour {
 
   void Update()
   {
-    // Vector3 dir = Vector3.zero;
-    // dir.x = Input.acceleration.x;
-    // dir.z = Input.acceleration.y;
+    Vector3 dir = Vector3.zero;
+    dir.x = Input.acceleration.x;
+    dir.z = Input.acceleration.y;
 
-    // if (dir.sqrMagnitude > 1) {
-    //   dir.Normalize();
-    // }
+    if (dir.sqrMagnitude > 1) {
+      dir.Normalize();
+    }
 
-    // Vector3 move = dir * movementVelocity * Time.deltaTime;
-    Vector3 move = this.transform.right * -Input.acceleration.y * movementVelocity * Time.deltaTime;
+    Vector3 move = dir * movementVelocity * Time.deltaTime;
+    // Vector3 move = this.transform.right * -Input.acceleration.y * movementVelocity * Time.deltaTime;
     this.transform.position += move;
 
-    Input.compass.enabled = true;
-    transform.rotation = Quaternion.Euler(0, Input.compass.trueHeading, 0);
+    // Input.compass.enabled = true;
+    // transform.rotation = Quaternion.Euler(0, Input.compass.trueHeading, 0);
   }
 }
